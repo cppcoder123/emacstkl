604f3e69fd353a4c127f004e83deb0677e9f9482
cppcoder123 <cppcoder123@users.noreply.github.com>
2026-06-08 19:00:03 +0200

    rules-mk: Enable i2c, disable mouse-keys

--- fw/rules.mk
+++ fw/rules.mk
@@ -5,7 +5,7 @@ F_CPU = 16000000
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = no       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
@@ -13,3 +13,4 @@ NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+I2C_ENABLE = yes
